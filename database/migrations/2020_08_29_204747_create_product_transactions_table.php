4b03cc317d0437d5996d6518320fd8ff25071c54
Tiago Ortiz <tiago.ortiz14@gmail.com>
2020-08-29 18:10:50 -0300

    Update ProductTransaction table / Tabela ProductTransaction atualizada

--- database/migrations/2020_08_29_204747_create_product_transactions_table.php
+++ database/migrations/2020_08_29_204747_create_product_transactions_table.php
@@ -14,8 +14,15 @@ class CreateProductTransactionsTable extends Migration
     public function up()
     {
         Schema::create('product_transactions', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('product_transaction_id');
+            $table->integer('product_id');            
+            $table->integer('quantity');
+            $table->string('type', 16); // If transaction was made by system or API
+            $table->dateTime('created_at');
             $table->timestamps();
+
+            // Add the foreign key and reference the product_id on product table
+            $table->foreign('product_id')->references('product_id')->on('product');
         });
     }
 
