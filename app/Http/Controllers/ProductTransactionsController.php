d43a211f20d0af2f649faab25bec566f9028c8ce
Tiago Ortiz <tiago.ortiz14@gmail.com>
2020-08-29 17:48:50 -0300

    Create the base files
    Added the Product and Product Transaction MVC base files

---
+++ app/Http/Controllers/ProductTransactionsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ProductTransactions;
+use Illuminate\Http\Request;
+
+class ProductTransactionsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ProductTransactions  $productTransactions
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProductTransactions $productTransactions)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ProductTransactions  $productTransactions
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProductTransactions $productTransactions)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ProductTransactions  $productTransactions
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProductTransactions $productTransactions)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ProductTransactions  $productTransactions
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProductTransactions $productTransactions)
+    {
+        //
+    }
+}
---
+++ database/factories/ProductTransactionsFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\ProductTransactions;
+use Faker\Generator as Faker;
+
+$factory->define(ProductTransactions::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
